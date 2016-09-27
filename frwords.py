e550c2def1229cfb249258689b8ae82b3d439f61
George Gosline <g.gosline@kew.org>
2016-09-27 17:29:49 +0100

    add pattern py3 conversion

--- frwords.py
+++ frwords.py
@@ -1,7 +1,32 @@
 import nltk
+from nltk import PunktSentenceTokenizer as pk
+from nltk.tokenize import WordPunctTokenizer
+from pattern.fr.inflect import singularize
+from pattern.fr import tokenize, parse
+from nltk.stem import SnowballStemmer
+
+import pickle
+import locale
+
+locale.setlocale(locale.LC_ALL, 'fra')
+
+tker = pk()
+tker = nltk.data.load('nltk:tokenizers/punkt/french.pickle')
+
+stemmer = SnowballStemmer('french')
+wpktk = WordPunctTokenizer()
 
 frphrases = open('frphrases.txt', encoding='utf-8')
 
+wset = set()
 for phrase in frphrases:
     desc = phrase.split('\t')[1]
-    print (desc)
+    #print (desc)
+    # print (tker.sentences_from_text(text=desc))
+    words = parse(desc)
+    wset.update(words)
+
+wlist = sorted(wset, key=locale.strxfrm)
+wstems = [(word, singularize(word), stemmer.stem(word)) for word in wlist]
+
+pass

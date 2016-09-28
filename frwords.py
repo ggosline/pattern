17e5297660e36f981e980262078362341f3035b4
George Gosline <g.gosline@kew.org>
2016-09-28 18:06:33 +0100

    french lexicon

--- frwords.py
+++ frwords.py
@@ -2,31 +2,41 @@ import nltk
 from nltk import PunktSentenceTokenizer as pk
 from nltk.tokenize import WordPunctTokenizer
 from pattern.fr.inflect import singularize
-from pattern.fr import tokenize, parse
-from nltk.stem import SnowballStemmer
+# from pattern.fr import tokenize, parse
+# from nltk.stem import SnowballStemmer
 
 import pickle
 import locale
+from csv import DictReader
 
 locale.setlocale(locale.LC_ALL, 'fra')
 
 tker = pk()
 tker = nltk.data.load('nltk:tokenizers/punkt/french.pickle')
 
-stemmer = SnowballStemmer('french')
+frengreader = DictReader(open('French-English-Lexicon.csv', encoding='windows-1252'))
+frlexicon = {row['French']: row for row in frengreader}
+
+# stemmer = SnowballStemmer('french')
 wpktk = WordPunctTokenizer()
 
-frphrases = open('frphrases.txt', encoding='utf-8')
+with  open('frphrases.txt', encoding='utf-8') as frphrases:
 
-wset = set()
-for phrase in frphrases:
-    desc = phrase.split('\t')[1]
-    #print (desc)
-    # print (tker.sentences_from_text(text=desc))
-    words = parse(desc)
-    wset.update(words)
+    wset = set()
+    for phrase in frphrases:
+        desc = phrase.split('\t')[1]
+        #print (desc)
+        # print (tker.sentences_from_text(text=desc))
+        words = wpktk.tokenize(desc)
+        wset.update([singularize(word) for word in words])
 
 wlist = sorted(wset, key=locale.strxfrm)
-wstems = [(word, singularize(word), stemmer.stem(word)) for word in wlist]
+wvocs = [(word, (frlexicon.get(singularize(word))['English'] if frlexicon.get(singularize(word)) else None)) for word in wlist]
+
+with open('frwords.txt', mode='w', encoding='utf-8') as frwords:
+    for wvoc in wvocs:
+        print(wvoc[0], wvoc[1], sep='\t', file=frwords)
+
+
 
 pass
